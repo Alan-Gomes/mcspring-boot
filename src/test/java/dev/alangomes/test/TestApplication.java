c1a3515b9f97e23b40c6e2c12e7bd8255f97c694
Alan Gomes <alangomes11@gmail.com>
2019-04-28 22:19:55 -0300

    Enable scheduling in tests

--- src/test/java/dev/alangomes/test/TestApplication.java
+++ src/test/java/dev/alangomes/test/TestApplication.java
@@ -2,9 +2,11 @@ package dev.alangomes.test;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "dev.alangomes.test")
 @EnableAutoConfiguration
+@EnableScheduling
 public class TestApplication {
 
 }
