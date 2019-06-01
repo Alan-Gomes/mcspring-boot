41b5b7321fdb1782e46055c20bacdf0ac4ce5654
Alan Gomes <alangomes11@gmail.com>
2019-06-01 00:10:17 -0300

    Add default converter from ConfigurationSection to Map

--- src/main/java/dev/alangomes/springspigot/configuration/ResourceConfiguration.java
+++ src/main/java/dev/alangomes/springspigot/configuration/ResourceConfiguration.java
@@ -1,6 +1,7 @@
 package dev.alangomes.springspigot.configuration;
 
 import lombok.val;
+import org.bukkit.configuration.ConfigurationSection;
 import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.beans.factory.config.DependencyDescriptor;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.core.env.Environment;
+
+import java.util.Map;
 
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
@@ -26,7 +29,9 @@ class ResourceConfiguration {
 
     @Bean
     public ConversionService conversionService() {
-        return new DefaultConversionService();
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(ConfigurationSection.class, Map.class, (s) -> s.getValues(true));
+        return conversionService;
     }
 
 }
