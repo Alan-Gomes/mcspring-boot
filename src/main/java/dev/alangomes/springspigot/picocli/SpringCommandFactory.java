bf1f0b891cd4902fa5a3161f49c0df04eaee6865
Alan Gomes <alangomes11@gmail.com>
2019-06-18 19:15:39 -0300

    Fix proxy support for annotated subcommands

--- src/main/java/dev/alangomes/springspigot/picocli/SpringCommandFactory.java
+++ src/main/java/dev/alangomes/springspigot/picocli/SpringCommandFactory.java
@@ -1,5 +1,7 @@
 package dev.alangomes.springspigot.picocli;
 
+import org.springframework.aop.framework.Advised;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,7 +15,11 @@ class SpringCommandFactory implements CommandLine.IFactory {
 
     @Override
     public <K> K create(Class<K> cls) throws Exception {
-        return beanFactory.getBean(cls);
+        K bean = beanFactory.getBean(cls);
+        if (!(bean instanceof Iterable) && AopUtils.isAopProxy(bean)) {
+            return (K) ((Advised) bean).getTargetSource().getTarget();
+        }
+        return bean;
     }
 
 }
